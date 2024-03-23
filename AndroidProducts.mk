72a892c1a5a789ee6eb1ef79612870f03dee7c26
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2024-03-23 15:10:16 +0100

    gta9pwifi: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: samsung Model: SM-X210 Script version: 3.0.0

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_gta9pwifi.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_gta9pwifi-user \
+    omni_gta9pwifi-userdebug \
+    omni_gta9pwifi-eng
